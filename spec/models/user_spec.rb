2a38f1a08c177328592299323f690e5a29c9b433
subratrout <subratnrout@gmail.com>
2018-02-19 22:12:20 -0700

    Generated Post resources

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Create an User" do
+    before do
+      @user = User.create(first_name: "subrat", last_name: "Rout", email: "test2@testmail.com", password: "password", password_confirmation: "password")
+    end
+    
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+
+    it "cannot be created without the first_name and last_name" do
+      @user.first_name = nil
+      @user.last_name = nil
+
+      expect(@user).to_not be_valid
+    end
+  end
 end
