25bea829cd96c73599813e373ceceefaa17a53ee
subratrout <subratnrout@gmail.com>
2018-03-15 17:19:38 -0600

    Customizing administrate admin dashboard

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -36,7 +36,7 @@ class UserDashboard < Administrate::BaseDashboard
     :posts,
     :id,
     :email,
-    :encrypted_password,
+    :type,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -45,9 +45,6 @@ class UserDashboard < Administrate::BaseDashboard
     :posts,
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
     :remember_created_at,
     :sign_in_count,
     :current_sign_in_at,
@@ -68,14 +65,6 @@ class UserDashboard < Administrate::BaseDashboard
     :posts,
     :email,
     :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
     :first_name,
     :last_name,
     :type,
