c414b38e43be44ff305b98b8352bfae3f684222f
subratrout <subratnrout@gmail.com>
2018-03-15 19:18:36 -0600

    Hide form elements from non_admin users

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -10,14 +10,23 @@ end
 describe "edit" do
   before do
     @post = FactoryGirl.create(:post)
+    visit edit_post_path(@post)
   end
 
-  it "has a status that can be edited on the form" do
-    visit edit_post_path(@post)
+  it "has a status that can be edited on the form by an Admin" do
     choose('post_status_approved')
 
     click_on "Save"
 
     expect(@post.reload.status).to eq("approved")
+  end
+
+  it "can not be edited by non admin" do
+    logout(:user)
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+
+    visit edit_post_path(@post)
+    expect(page).to_not have_content("Approved")
   end
 end
