c2560414f62ac9f1debe4e2a4ce502b27c1d35b2
subratrout <subratnrout@gmail.com>
2018-03-15 18:23:21 -0600

    Customized administrate to show enum status on dashboard and added AdminUser to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Subrat", last_name: "Rout")
-
+AdminUser.create(email: "admin@test.com", password: "password", password_confirmation: "password", first_name:"Admin", last_name: "User")
 puts "1 User created"
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
