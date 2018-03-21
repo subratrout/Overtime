c6b27a9e3e3969248b924efc0fe081002b9eb46c
subratrout <subratnrout@gmail.com>
2018-03-20 20:27:48 -0600

    Created FactoryGirl audit_log and added seeds to seeds.db

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :audit_log do
-    user nil
-    status 1
-    start_date "2018-03-20"
-    end_date "2018-03-20"
+    user
+    status 0
+    start_date (Date.today - 6.days)
+    end_date nil
   end
 end
