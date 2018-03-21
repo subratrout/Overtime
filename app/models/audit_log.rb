f01cd3636c01273f88f40d3af02d1a8a90b40016
subratrout <subratnrout@gmail.com>
2018-03-21 15:31:05 -0600

    Audit_log data validations with RSpec

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,10 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+  validates_presence_of :user_id, :status, :start_date
+  after_initialize :set_default
+
+  private
+    def set_default
+      self.start_date ||= Date.today - 6.days
+    end
 end
