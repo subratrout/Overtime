7f4a2ba8dcfe5e2d1f736f5c028fbdcbb25d8ca1
subratrout <subratnrout@gmail.com>
2018-03-21 16:25:26 -0600

    Implemented status enum and label generator for audit items

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -2,6 +2,7 @@ class AuditLog < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :status, :start_date
   after_initialize :set_default
+  enum status: { pending: 0, confirmed: 1}
 
   private
     def set_default
