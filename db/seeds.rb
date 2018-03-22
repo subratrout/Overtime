d2144763179f267b5e3dd9cc75818035735a6a8f
subratrout <subratnrout@gmail.com>
2018-03-21 18:16:59 -0600

    Bringing live data to admin management dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
 AdminUser.create(email: "admin@test.com", password: "password", password_confirmation: "password", first_name:"Admin", last_name: "User", phone: "5402355002")
 puts "1 User created"
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been greated"
