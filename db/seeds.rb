20385363d16f7043352ea6b2ef1362fd15f009d1
subratrout <subratnrout@gmail.com>
2018-03-23 00:41:11 -0600

    Changed seed file to create audit_log before posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,16 @@
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Subrat", last_name: "Rout", phone: "5402828076")
 AdminUser.create(email: "admin@test.com", password: "password", password_confirmation: "password", first_name:"Admin", last_name: "User", phone: "5402355002")
 puts "1 User created"
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
-end
-
-puts "100 Posts have been greated"
 
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
+end
+
+puts "100 Posts have been greated"
+
