6cfa910c038eadbc9a7b3f6b95af50bde33175e6
subratrout <subratnrout@gmail.com>
2018-03-24 22:19:13 -0600

    Used before_update call back to set end date for post

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -3,9 +3,14 @@ class AuditLog < ApplicationRecord
   validates_presence_of :user_id, :status, :start_date
   after_initialize :set_default
   enum status: { pending: 0, confirmed: 1}
+  before_update :set_end_date, if: :confirmed?
 
   private
     def set_default
       self.start_date ||= Date.today - 6.days
+    end
+
+    def set_end_date
+      self.end_date = Date.today
     end
 end
